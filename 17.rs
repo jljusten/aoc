0507ba45132587e9e730576c7ec67014438f545e
Jordan Justen <jljusten@gmail.com>
2017-12-17 01:09:44 -0800

    AoC 2017: Day 17 (part 1, take 2)
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- 17.rs
+++ 17.rs
@@ -3,20 +3,44 @@
 /* Jordan Justen : this file is public domain */
 
 fn main() {
+    const COUNT: usize = 50000000;
+    let mut prev = Vec::new();
+    let mut next = Vec::new();
+    prev.reserve(2018.min(COUNT + 1));
+    next.reserve(2018.min(COUNT + 1));
+    prev.push(0);
+    next.push(0);
+
     let input = read_input1();
-    let mut state = vec!(0u32; 1);
-    state.reserve(50000000);
     let mut pos = 0usize;
+    let mut idx = 0usize;
     let mut len = 1;
 
-    for i in 1 .. 2018 {
-        pos = (pos + input) % len;
-        state.insert(pos + 1, i);
-        pos += 1;
+    for i in 1 .. 2018.min(COUNT + 1) {
+        let next_idx = (idx + input) % len;
+        if next_idx > idx {
+            for _ in 0 .. next_idx - idx {
+                pos = next[pos];
+            }
+        } else {
+            for _ in 0 .. idx - next_idx {
+                pos = prev[pos];
+            }
+        }
+        idx = next_idx;
+        prev.push(0);
+        next.push(0);
+        prev[i] = pos;
+        let new_node_next = next[pos];
+        next[i] = new_node_next;
+        next[pos] = i;
+        prev[new_node_next] = i;
+        pos = i;
+        idx += 1;
         len += 1;
     }
 
-    println!("part1: {}", state[pos + 1]);
+    println!("part1: {}", next[pos]);
 }
 
 #[allow(dead_code)]
