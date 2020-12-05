d2b981a8475c174b2876f463d71df5f6550f5c86
Jordan Justen <jljusten@gmail.com>
2020-12-05 01:13:02 -0800

    AoC 2020: Day 2: Part 1
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

---
+++ 02.rs
@@ -0,0 +1,54 @@
+/* Advent of Code 2020, Day 2 */
+
+/* Jordan Justen : this file is public domain */
+
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::str::FromStr;
+
+pub fn main() {
+    let input = read_input();
+    let num_valid = input.iter().filter(|i| is_valid(i)).count();
+    println!("Part 1:");
+    println!("num_valid: {}", num_valid);
+}
+
+fn is_valid(pw: &Input) -> bool {
+    let count = pw.pw.chars().filter(|p| *p == pw.ch).count();
+    count >= pw.min && count <= pw.max
+}
+
+#[derive(Debug)]
+struct Input {
+    min: usize,
+    max: usize,
+    ch: char,
+    pw: String,
+}
+
+fn line_to_input(line: &String) -> Input {
+    let mut split = line.split_whitespace();
+    let range = split.next().unwrap();
+    let char_str = split.next().unwrap();
+    let char_str = &char_str[..char_str.len()-1];
+    let pw = split.next().unwrap();
+    let mut range_split = range.split('-');
+    Input {
+        min: usize::from_str(range_split.next().unwrap()).unwrap(),
+        max: usize::from_str(range_split.next().unwrap()).unwrap(),
+        ch: char::from_str(char_str).unwrap(),
+        pw: pw.clone().to_string(),
+    }
+}
+
+fn read_input() -> Vec<Input> {
+    let fname = "02.input";
+    let input_f = File::open(fname).unwrap();
+    let input_file = BufReader::new(&input_f);
+    input_file
+        .lines()
+        .enumerate()
+        .map(|line| line_to_input(&line.1.unwrap()))
+        .collect()
+}
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@ My Solutions to [Advent of Code 2020](https://adventofcode.com/2020)
 using [Rust](https://www.rust-lang.org/)
 
 1. [Day 1](https://adventofcode.com/2020/day/1): [01.rs](01.rs)
+2. [Day 2](https://adventofcode.com/2020/day/2): [02.rs](02.rs)
