b40780aa2fa852b6a67fb1a43796a8d4868b3b3d
Jordan Justen <jljusten@gmail.com>
2020-12-16 02:34:46 -0800

    AoC 2020: Day 11: Part 2
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- 11.rs
+++ 11.rs
@@ -10,9 +10,11 @@ use std::io::BufReader;
 pub fn main() {
     let input = read_input("11.test");
     assert_eq!(part1(&input), 37);
+    assert_eq!(part2(&input), 26);
 
     let input = read_input("11.input");
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &Input) -> usize {
@@ -86,6 +88,87 @@ fn count_occupied(input: &Input) -> usize {
         }
     }
     occupied_count
+}
+
+fn part2(input: &Input) -> usize {
+    let mut state = (*input).clone();
+    loop {
+        let changed = next_state_part2(&mut state);
+        if !changed {
+            break;
+        }
+    }
+    count_occupied(&state)
+}
+
+fn next_state_part2(input: &mut Input) -> bool {
+    let counts = count_surrounding_occupied_part2(input);
+
+    let mut changed = false;
+    let (h, w) = (input.data.len(), input.data[0].len());
+    let next = input;
+    for y in 0..h {
+        for x in 0..w {
+            match next.data[y][x] {
+                SquareState::Floor => (),
+                SquareState::Empty => {
+                    if counts[y][x] == 0 {
+                        next.data[y][x] = SquareState::Occupied;
+                        changed = true;
+                    }
+                }
+                SquareState::Occupied => {
+                    if counts[y][x] >= 5 {
+                        next.data[y][x] = SquareState::Empty;
+                        changed = true;
+                    }
+                }
+            }
+        }
+    }
+    changed
+}
+
+static DIRS: [(isize, isize); 8] = [
+    (-1, 0),
+    (-1, 1),
+    (0, 1),
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, -1),
+    (-1, -1),
+];
+
+fn count_surrounding_occupied_part2(input: &Input) -> Vec<Vec<u8>> {
+    let d = &input.data;
+    let (h, w) = (d.len(), d[0].len());
+    let mut counts = vec![vec!(0u8; w); h];
+    for y in 0..h {
+        for x in 0..w {
+            let mut occupied_count = 0;
+            for dir in DIRS.iter() {
+                let (mut x, mut y) = (x as isize, y as isize);
+                loop {
+                    x += dir.0;
+                    y += dir.1;
+                    if x < 0 || x >= w as isize || y < 0 || y >= h as isize {
+                        break;
+                    }
+                    match d[y as usize][x as usize] {
+                        SquareState::Occupied => {
+                            occupied_count += 1;
+                            break;
+                        },
+                        SquareState::Empty => break,
+                        _ => (),
+                    }
+                }
+            }
+            counts[y][x] = occupied_count;
+        }
+    }
+    counts
 }
 
 #[derive(Clone, Debug, PartialEq)]
