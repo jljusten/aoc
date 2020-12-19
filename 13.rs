46c50ec1b93c2805ca8d8cbf3b6a5b178e8277e8
Jordan Justen <jljusten@gmail.com>
2020-12-19 12:32:17 -0800

    AoC 2020: Day 13: Part 2
    Signed-off-by: Jordan Justen <jljusten@gmail.com>

--- 13.rs
+++ 13.rs
@@ -11,9 +11,23 @@ use std::str::FromStr;
 pub fn main() {
     let input = read_input("13.test");
     assert_eq!(part1(&input), 295);
+    assert_eq!(part2(&input), 1068781);
+
+    static MORE_PART2_EXAMPLES: [(&str, u64); 5] = [
+        ("17,x,13,19", 3417),
+        ("67,7,59,61", 754018),
+        ("67,x,7,59,61", 779210),
+        ("67,7,x,59,61", 1261476),
+        ("1789,37,47,1889", 1202161486),
+    ];
+    for e in MORE_PART2_EXAMPLES.iter() {
+        let input = input_from_str(["0\n", e.0, "\n"].concat().as_str());
+        assert_eq!(part2(&input), e.1);
+    }
 
     let input = read_input("13.input");
     println!("Part 1: {}", part1(&input));
+    println!("Part 2: {}", part2(&input));
 }
 
 fn part1(input: &Input) -> usize {
@@ -28,6 +42,27 @@ fn part1(input: &Input) -> usize {
                 s.1 = wait * x;
             }
             Some(s.1)
+        })
+        .last()
+        .unwrap()
+}
+
+fn part2(input: &Input) -> u64 {
+    input
+        .nums
+        .iter()
+        .scan(vec![(1, 0)], |terms, &x| {
+            let mod_target = (x.0 - (x.1 % x.0)) % x.0;
+            let mut res = 0u64;
+            for i in 0.. {
+                let eval = terms.iter().rev().fold(i, |a, x| a * x.0 + x.1);
+                if (eval % x.0) == mod_target {
+                    res = eval as u64;
+                    terms.push((x.0, i));
+                    break;
+                }
+            }
+            Some(res)
         })
         .last()
         .unwrap()
